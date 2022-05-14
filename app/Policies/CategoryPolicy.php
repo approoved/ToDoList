f0c4d3f73148a6425f1e35323f820bce16bd2380
admin <admin@MacBook-Air-admin.local>
2022-05-14 22:55:13 +0300

    task category added

---
+++ app/Policies/CategoryPolicy.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Category;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+final class CategoryPolicy
+{
+    use HandlesAuthorization;
+
+    public function manage(Authenticatable $user, Category $category): bool
+    {
+        return $user->id === $category->user_id;
+    }
+
+    public function show(Authenticatable $user, Category $category): bool
+    {
+        return $this->manage($user, $category);
+    }
+
+    public function update(Authenticatable $user, Category $category): bool
+    {
+        return $this->manage($user, $category);
+    }
+
+    public function destroy(Authenticatable $user, Category $category): bool
+    {
+        return $this->manage($user, $category);
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,12 +2,16 @@
 
 namespace App\Providers;
 
+use App\Models\Category;
 use Laravel\Passport\Passport;
+use App\Policies\CategoryPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 final class AuthServiceProvider extends ServiceProvider
 {
-    protected $policies = [];
+    protected $policies = [
+        Category::class => CategoryPolicy::class,
+    ];
 
     public function boot(): void
     {
